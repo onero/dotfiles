bc0caf5f163961ab873f2f42b94b393cee9479d3
Adam Hansen <onero@users.noreply.github.com>
2022-07-28 18:50:51 +0200

    Updated symlinks

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source helper.sh
 
+# Ensure files are removed
+rm -rf $HOME/.zshrc
+rm -rf $HOME/.gitconfig
+rm -rf $HOME/.vimrc
+
+# Create symbolic links
 ln $DOTFILES/.zshrc ~/.zshrc
 ln $DOTFILES/.gitconfig ~/.gitconfig
 ln $DOTFILES/.vimrc ~/.vimrc
