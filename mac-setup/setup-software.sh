defecf6dc7d38addc8d2a03f30999560c6e90150
Adam Hansen <onero@users.noreply.github.com>
2022-07-28 18:49:26 +0200

    Updated mac setup

--- mac-setup/setup-software.sh
+++ mac-setup/setup-software.sh
@@ -4,10 +4,9 @@
 xcode-select --install
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Install software in Brewfile
-brew bundle --file ~/.dotfiles/mac-setup/Brewfile
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Update App Store apps
 sudo softwareupdate -i -a
@@ -15,6 +14,9 @@ sudo softwareupdate -i -a
 # Update Homebrew (Cask) & packages
 brew update
 brew upgrade
+
+# Install software in Brewfile
+brew bundle --file ~/.dotfiles/mac-setup/Brewfile
 
 # Update npm & packages
 npm install npm -g
